e04b1cd54a012b19b21a44d43be6715c797d304d
Kanakoo <1459035851@qq.com>
2017-05-11 11:59:25 +0800

    msg

--- json/jsonp.js
+++ json/jsonp.js
@@ -5,11 +5,11 @@ var  http  =  require('http');
 var url=require('url');
 var querystring=require('querystring');
 http.createServer(function (req,res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+  //  res.setHeader("Access-Control-Allow-Origin", "*");
     var query = url.parse(req.url,true).query.cb;
     console.log(query);
-    res.write('abc');
-    res.end(query+'()');
+   // res.write('abc');
+    res.end(query+'("data")');
     /*var qs = querystring.parse(req.url.split('?')[1]);
     if(qs.cb) {
         res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
