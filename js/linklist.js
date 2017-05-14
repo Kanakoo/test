84878f2c13cd6aed3222ddecbc76deaeef7e279f
Kanakoo <1459035851@qq.com>
2017-05-14 18:02:04 +0800

    msg

--- js/linklist.js
+++ js/linklist.js
@@ -10,10 +10,44 @@ function LinkList(){
 }
 LinkList.prototype={
     constructor:LinkList,
-    preAdd:function(value){
+    constructList:function(arr){
+        var rear=this.head;
+        for(var i=0;i<arr.length;i++){
+            var s=new node(arr[i]);
+            rear.next=s;
+            rear=s;
+        }
+    },
+    addByIndex:function(value,index){
+        var pivot=this.findByIndex(index);
         var newnode=new node(value);
-        newnode.next=this.head.next;
-        this.head.next=newnode;
+        newnode.next=pivot.next;
+        pivot.next=newnode;
+    },
+    addByValue:function(value,pivot){
+        var current=this.findByValue(pivot);
+        var newnode=new node(value);
+        newnode.next=current.next;
+        current.next=newnode;
+    },
+    findByValue:function(v){
+        var current=this.head;
+        while(current.value!==v){
+            current=current.next;
+        }
+        return current;
+    },
+    findByIndex:function(index){
+        var current=this.head.next;
+        for(var i=0;i<index;i++){
+            current=current.next;
+        }
+        return current;
+    },
+    deleteByIndex:function (index) {
+        var current=this.findByIndex(index);
+        var pre=this.findByIndex(index-1);
+        pre.next=current.next;
     },
     print:function(){
         var current = this.head;
@@ -24,9 +58,22 @@ LinkList.prototype={
             current = current.next;
         }
         console.log(arr);
+    },
+    reverse:function () {
+        var p=this.head.next;
+        this.head.next=null;
+        while(p!=null){
+            var q=p;
+            p=p.next;
+            q.next=this.head.next;
+            this.head.next=q;
+        }
     }
-}
+};
 var list=new LinkList();
-list.preAdd(1);
-list.preAdd(2);
+list.constructList([1,2,3]);
+list.addByIndex('abc',1);
+list.addByValue('abc',1);
+list.deleteByIndex(1);
+list.reverse();
 list.print();
